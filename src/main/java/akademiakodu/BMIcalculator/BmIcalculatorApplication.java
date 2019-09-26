c0d50d0a39bfeb1507946842877e3b5f49f5512a
Rafal <rafaljaneczek@o2.pl>
2019-09-26 22:22:16 +0200

    initial commit

---
+++ src/main/java/akademiakodu/BMIcalculator/BmIcalculatorApplication.java
@@ -0,0 +1,13 @@
+package akademiakodu.BMIcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmIcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmIcalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/akademiakodu/BMIcalculator/BmIcalculatorApplicationTests.java
@@ -0,0 +1,16 @@
+package akademiakodu.BMIcalculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BmIcalculatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
