d53fae62dc6dfa46aff7fea2fdca72eb0534a034
Rafal <rafaljaneczek@o2.pl>
2019-09-29 11:40:54 +0200

    added UserRepository interface

---
+++ src/main/java/akademiakodu/BMIcalculator/Repository/UserRepository.java
@@ -0,0 +1,15 @@
+package akademiakodu.BMIcalculator.Repository;
+
+
+import akademiakodu.BMIcalculator.Model.User;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Primary
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByEmail(String email);
+
+}
