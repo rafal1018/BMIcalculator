2dd5adc42f4d476a23a0a3873e17559f31c1e56a
Rafal <rafaljaneczek@o2.pl>
2019-09-30 20:39:52 +0200

    added login method in LoginController class

--- src/main/java/akademiakodu/BMIcalculator/Controller/LoginController.java
+++ src/main/java/akademiakodu/BMIcalculator/Controller/LoginController.java
@@ -1,4 +1,21 @@
 package akademiakodu.BMIcalculator.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class LoginController {
+
+    @Autowired
+    private Service service;
+
+    @GetMapping(value = {"/", "/login"})
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
 }
