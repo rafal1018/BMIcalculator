b059b83a7c62e629d6e10d644ba713d05757bd35
Rafal <rafaljaneczek@o2.pl>
2019-10-03 21:02:16 +0200

    Resut class added

---
+++ src/main/java/akademiakodu/BMIcalculator/Model/Result.java
@@ -0,0 +1,30 @@
+package akademiakodu.BMIcalculator.Model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+
+@Data
+@Entity
+@Table(name = "users_result")
+public class Result {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "result_id")
+    private Integer id;
+
+    @Column(name = "height")
+    private Double height;
+
+    @Column(name = "weight")
+    private Double weight;
+
+    @Column(name = "result")
+    private Double result;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
