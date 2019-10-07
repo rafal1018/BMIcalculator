b9802d2e098da7d744e696a18d411714c239ff8e
Rafal <rafaljaneczek@o2.pl>
2019-10-07 22:20:51 +0200

    added getBMI method in BMIController class

--- src/main/java/akademiakodu/BMIcalculator/Controller/BMIController.java
+++ src/main/java/akademiakodu/BMIcalculator/Controller/BMIController.java
@@ -1,5 +1,6 @@
 package akademiakodu.BMIcalculator.Controller;
 
+import akademiakodu.BMIcalculator.Model.Result;
 import akademiakodu.BMIcalculator.Model.User;
 import akademiakodu.BMIcalculator.UserService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,48 +11,52 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+
 @Controller
 public class BMIController {
 
     @Autowired
     private UserService userService;
 
+    private User user;
+
     @RequestMapping(value = "/admin/home", method = RequestMethod.GET)
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView();
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User user = userService.findUserByEmail(auth.getName());
-        modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
-        modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
+        User getUser = userService.findUserByEmail(auth.getName());
+        this.user = getUser;
+        modelAndView.addObject("userName", "Welcome " + getUser.getName() + " " + getUser.getLastName());
         modelAndView.setViewName("admin/home");
         return modelAndView;
     }
 
-//    @RequestMapping(value = "/admin/home", method = RequestMethod.GET)
-//    public ModelAndView getBMIParam(@RequestParam(value = "weight", required = false) Double weight,
-//                                    @RequestParam(value = "height", required = false)Double height,
-//                                    BindingResult validator){
-//        ModelAndView modelAndView = new ModelAndView();
-//
-//        if (weight != null && height != null) {
-//            result.setUser(user);
-//            result.setResult(weight / (height * height));
-//            modelAndView.addObject("BMIresult", result.getResult());
-//            try {
-//                userService.addResult(result);
-//            } catch (Exception e) {
-//                System.out.print(e);
-//            }
-//        }
-//        if (!validator.hasFieldErrors()) {
-//            modelAndView.addObject("add", "yes");
-//            modelAndView.addObject("result", result);
-//        } else {
-//            modelAndView.addObject("result", result);
-//            modelAndView.addObject("error", "yes");
-//        }
-//
-//        modelAndView.setViewName("admin/home");
-//        return modelAndView;
-//    }
+    @RequestMapping(value = "/admin/home", method = RequestMethod.GET)
+    public ModelAndView getBMIParam(@RequestParam(value = "weight", required = false) Double weight,
+                                    @RequestParam(value = "height", required = false)Double height,
+                                    @Valid Result result, BindingResult validator){
+        ModelAndView modelAndView = new ModelAndView();
+
+        if (weight != null && height != null) {
+            result.setUser(user);
+            result.setResult(weight / (height * height));
+            modelAndView.addObject("BMIresult", result.getResult());
+            try {
+                userService.addResult(result);
+            } catch (Exception e) {
+                System.out.print(e);
+            }
+        }
+        if (!validator.hasFieldErrors()) {
+            modelAndView.addObject("add", "yes");
+            modelAndView.addObject("result", result);
+        } else {
+            modelAndView.addObject("result", result);
+            modelAndView.addObject("error", "yes");
+        }
+
+        modelAndView.setViewName("admin/home");
+        return modelAndView;
+    }
 }
--- src/main/java/akademiakodu/BMIcalculator/Model/Result.java
+++ src/main/java/akademiakodu/BMIcalculator/Model/Result.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import javax.persistence.*;
 
 
-@Data
 @Entity
 @Table(name = "users_result")
 public class Result {
@@ -27,4 +26,44 @@ public class Result {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getResult() {
+        return result;
+    }
+
+    public void setResult(Double result) {
+        this.result = result;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
